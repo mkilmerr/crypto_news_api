8497f1f6ee47e6ef625624661ddfbd263af65172
Marcos <Kilmer>
2019-09-26 12:34:11 -0300

    object format

--- src/controllers/getLatestNews.js
+++ src/controllers/getLatestNews.js
@@ -3,6 +3,7 @@ const Api = new CryptoNewsAPI('b8d3767b03073fb38780eb1de50198ea');
 const ProxyApi = new CryptoNewsAPI('b8d3767b03073fb38780eb1de50198ea', 'http://cryptocontrol_proxy/api/v1/public');
 Api.enableSentiment();
 
+
 const infosSelect = {
     title:[],
     description:[],
@@ -15,18 +16,14 @@ module.exports = {
     getLatestNews : async (req, res) => {
         const latestNews = await Api.getLatestNews(language='po');
         try {
-           latestNews.forEach(info => {
-               infosSelect.title.push(info.title);
-               infosSelect.description.push(info.description);
-               infosSelect.source.favicon.push(info.source.favicon);
-               infosSelect.url.push(info.url);
-           });
+            res.send(latestNews)
         }catch(err){
             res.send(err)
         }
-        const arr = []
-        arr.push(infosSelect)
-        res.send(arr)
+       
+        return res.send(latestNews)
     },
-
+    getAllNews : (req,res) => {
+        
+    }
 }
